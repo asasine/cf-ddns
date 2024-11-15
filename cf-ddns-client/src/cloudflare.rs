f11cfe138f115e31cd94beb43920ac84cb60bba9
Adam Sasine <adam.sasine@gmail.com>
2024-11-15 18:06:05 -0500

    Added record updating to client

--- cf-ddns-client/src/cloudflare.rs
+++ cf-ddns-client/src/cloudflare.rs
@@ -2,6 +2,9 @@
 
 use std::error;
 use std::fmt;
+use std::net::IpAddr;
+use std::net::Ipv4Addr;
+use std::net::Ipv6Addr;
 
 use cf_ddns::ResponseInfo;
 use reqwest::{
@@ -10,19 +13,87 @@ use reqwest::{
 };
 
 use serde::Deserialize;
+use serde::Serialize;
 
 type CfResponseInfo = ResponseInfo<i32>;
 
 #[derive(Deserialize)]
-struct PaginatedResponse<T> {
+struct ListResponse<T> {
     success: bool,
     errors: Vec<CfResponseInfo>,
     result: Vec<T>,
 }
 
 #[derive(Deserialize)]
-struct IdResult {
+struct Response<T> {
+    success: bool,
+    errors: Vec<CfResponseInfo>,
+    result: T,
+}
+
+#[derive(Deserialize)]
+struct Id {
     id: String,
+}
+
+/// A DNS record.
+#[derive(Deserialize, Serialize, Clone)]
+#[serde(tag = "type")]
+pub struct Record {
+    /// The record ID.
+    pub id: String,
+
+    /// The name of the zone.
+    pub zone_name: String,
+
+    /// The name of the record.
+    pub name: String,
+
+    /// The content of the record.
+    #[serde(flatten)]
+    pub content: RecordContent,
+}
+
+/// The content of a DNS record.
+#[derive(Debug, Deserialize, Serialize, Clone)]
+#[serde(tag = "type")]
+pub enum RecordContent {
+    /// An `A` record.
+    A {
+        /// The IPv4 address of the record.
+        content: Ipv4Addr,
+    },
+
+    /// An `AAAA` record.
+    AAAA {
+        /// The IPv6 address of the record.
+        content: Ipv6Addr,
+    },
+
+    /// Another type of record.
+    #[serde(other)]
+    Other,
+}
+
+#[derive(Serialize)]
+struct UpdateRecord {
+    content: String,
+    r#type: String,
+}
+
+impl From<IpAddr> for UpdateRecord {
+    fn from(ip: IpAddr) -> Self {
+        match ip {
+            IpAddr::V4(ip) => UpdateRecord {
+                content: ip.to_string(),
+                r#type: "A".to_string(),
+            },
+            IpAddr::V6(ip) => UpdateRecord {
+                content: ip.to_string(),
+                r#type: "AAAA".to_string(),
+            },
+        }
+    }
 }
 
 /// A client for interacting with the Cloudflare API.
@@ -32,7 +103,7 @@ pub struct Cloudflare {
 
 /// Errors that can occur when interacting with the Cloudflare API.
 #[derive(Debug)]
-pub enum CloudflareError {
+pub enum CloudflareError<T> {
     /// An error occurred while sending a request to the Cloudflare API.
     RequestFailed(reqwest::Error),
 
@@ -44,33 +115,76 @@ pub enum CloudflareError {
 
     /// The response from the Cloudflare API was empty.
     EmptyResult,
+
+    /// An error occurred that is specific to the Cloudflare API being called.
+    ApiSpecific(T),
 }
 
-impl fmt::Display for CloudflareError {
+impl<T: fmt::Display> fmt::Display for CloudflareError<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Self::RequestFailed(e) => write!(f, "Failed to send request: {e}."),
             Self::ResponseNotJson(e) => write!(f, "Failed to parse JSON response: {e}."),
             Self::Error(errors) => {
-                let errors = errors
-                    .iter()
-                    .map(|info| format!("{info:?}"))
-                    .collect::<Vec<String>>()
-                    .join("; ");
-
-                write!(f, "{errors}")
+                if errors.is_empty() {
+                    write!(f, "The Cloudflare API returned an unknown error.")
+                } else if errors.len() == 1 {
+                    write!(f, "The Cloudflare API returned an error: {errors:?}")
+                } else {
+                    write!(f, "The Cloudflare API returned multiple errors: {errors:?}")
+                }
             }
             Self::EmptyResult => write!(f, "No results found."),
+            Self::ApiSpecific(inner) => write!(f, "{inner}"),
         }
     }
 }
 
-impl error::Error for CloudflareError {
+impl<T: fmt::Display + fmt::Debug> error::Error for CloudflareError<T> {
     fn source(&self) -> Option<&(dyn error::Error + 'static)> {
         match self {
             Self::RequestFailed(e) | Self::ResponseNotJson(e) => Some(e),
-            Self::Error(_) | Self::EmptyResult => None,
+            Self::Error(_) | Self::EmptyResult | Self::ApiSpecific(_) => None,
         }
+    }
+}
+
+/// No additional API-specific errors.
+#[derive(Debug)]
+pub struct NoApiSpecific;
+
+impl fmt::Display for NoApiSpecific {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "An unknown error occurred.")
+    }
+}
+
+impl error::Error for NoApiSpecific {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        None
+    }
+}
+
+/// Errors that can occur when getting a record ID.
+#[derive(Debug)]
+pub enum GetRecordIdError {
+    /// The record type is not supported.
+    InvalidRecordType(String),
+}
+
+impl fmt::Display for GetRecordIdError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::InvalidRecordType(record_type) => {
+                write!(f, "The record type '{}' is not supported.", record_type)
+            }
+        }
+    }
+}
+
+impl error::Error for GetRecordIdError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        None
     }
 }
 
@@ -85,16 +199,15 @@ impl Cloudflare {
     }
 
     /// Get the zone ID of a Cloudflare DNS zone by name.
-    pub fn get_zone_id(&self, name: &str) -> Result<String, CloudflareError> {
-        let response: PaginatedResponse<IdResult> = self
+    pub fn get_zone_id(&self, name: &str) -> Result<String, CloudflareError<NoApiSpecific>> {
+        let response = self
             .client
             .get(format!(
-                "https://api.cloudflare.com/client/v4/zones?name={}",
-                name
+                "https://api.cloudflare.com/client/v4/zones?name={name}"
             ))
             .send()
             .map_err(CloudflareError::RequestFailed)?
-            .json::<PaginatedResponse<IdResult>>()
+            .json::<ListResponse<Id>>()
             .map_err(CloudflareError::ResponseNotJson)?;
 
         if !response.success {
@@ -107,5 +220,95 @@ impl Cloudflare {
             .ok_or(CloudflareError::EmptyResult)?
             .id
             .clone())
+    }
+
+    /// Get the record ID of a Cloudflare DNS record by name.
+    pub fn get_record_id(
+        &self,
+        zone_id: &str,
+        name: &str,
+    ) -> Result<String, CloudflareError<GetRecordIdError>> {
+        let response = self
+            .client
+            .get(format!("https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records?name.exact={name}"))
+            .send()
+            .map_err(CloudflareError::RequestFailed)?
+            .json::<ListResponse<Record>>()
+            .map_err(CloudflareError::ResponseNotJson)?;
+
+        if !response.success {
+            return Err(CloudflareError::Error(response.errors));
+        }
+
+        Ok(response
+            .result
+            .first()
+            .ok_or(CloudflareError::EmptyResult)?
+            .id
+            .clone())
+    }
+
+    /// Update a DNS record with the new content from the given `content`.
+    ///
+    /// This performs a `PATCH` request to the Cloudflare API. Only the type and value of the record is updated.
+    pub fn update_record(
+        &self,
+        zone_id: &str,
+        record_id: &str,
+        content: IpAddr,
+    ) -> Result<Record, CloudflareError<NoApiSpecific>> {
+        let request_body = UpdateRecord::from(content);
+        let response = self
+            .client
+            .patch(format!(
+                "https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records/{record_id}",
+            ))
+            .json(&request_body)
+            .send()
+            .map_err(CloudflareError::RequestFailed)?
+            .json::<Response<Record>>()
+            .map_err(CloudflareError::ResponseNotJson)?;
+
+        if !response.success {
+            return Err(CloudflareError::Error(response.errors));
+        }
+
+        Ok(response.result)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn deserialize_a_record() {
+        let json =
+            r#"{"type":"A","id":"123","name":"foo","zone_name":"zone","content":"10.0.0.0"}"#;
+        let record: Record = serde_json::from_str(json).unwrap();
+        match record.content {
+            RecordContent::A { content: ip } => assert_eq!(ip, Ipv4Addr::new(10, 0, 0, 0)),
+            _ => panic!("Expected an A record."),
+        }
+    }
+
+    #[test]
+    fn deserialize_aaaa_record() {
+        let json = r#"{"type":"AAAA","id":"123","name":"foo","zone_name":"zone","content":"::1"}"#;
+        let record: Record = serde_json::from_str(json).unwrap();
+        match record.content {
+            RecordContent::AAAA { content: ip } => assert_eq!(ip, Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1)),
+            _ => panic!("Expected an AAAA record."),
+        }
+    }
+
+    #[test]
+    fn deserialize_other_record() {
+        let json = r#"{"type":"TXT","id":"123","name":"foo","zone_name":"zone","content":"example"}"#;
+        let record: Record = serde_json::from_str(json).unwrap();
+        match record.content {
+            RecordContent::Other => {}
+            _ => panic!("Expected an other record."),
+        }
     }
 }
--- cf-ddns-client/src/main.rs
+++ cf-ddns-client/src/main.rs
@@ -18,18 +18,39 @@ struct Args {
     /// The API token to authenticate with the Cloudflare API.
     #[arg(long)]
     token: String,
+
+    /// The DNS record to update.
+    #[command(flatten)]
+    record: RecordArgs,
+
+    /// Include debug output.
+    #[arg(long)]
+    debug: bool,
+}
+
+#[derive(clap::Args)]
+struct ZoneArgs {
+    /// The name of the Cloudflare DNS zone.
+    #[arg(long)]
+    zone_name: String,
+
+    /// The ID of the Cloudflare DNS zone, if known,
+    ///
+    /// This can be provided to avoid the need to look up the zone ID by name. It should match the ID of [`Self::zone_name`].
+    #[arg(long)]
+    zone_id: Option<String>,
 }
 
 #[derive(clap::Args)]
 #[group(required = true, multiple = false)]
-struct ZoneArgs {
-    /// The name of the Cloudflare DNS zone.
-    #[arg(long, group = "zone")]
-    zone_name: Option<String>,
+struct RecordArgs {
+    /// The name of the record to update.
+    #[arg(long)]
+    record_name: Option<String>,
 
-    /// The ID of the Cloudflare DNS zone. This is an alternative to `zone_name`.
-    #[arg(long, group = "zone")]
-    zone_id: Option<String>,
+    /// The ID of the record to update, instead of looking up from the name.
+    #[arg(long)]
+    record_id: Option<String>,
 }
 
 fn main() -> ExitCode {
@@ -43,16 +64,53 @@ fn main() -> ExitCode {
         }
     };
 
-    println!("IP: {}", ip);
+    if args.debug {
+        eprintln!("IP: {}", ip);
+    }
 
     let client = Cloudflare::try_new(&args.token).unwrap();
-    let zone_id = match (args.zone.zone_name, args.zone.zone_id) {
-        (Some(zone_name), _) => client.get_zone_id(&zone_name).unwrap(),
-        (_, Some(zone_id)) => zone_id,
-        _ => unreachable!("Either zone_name or zone_id must be provided."),
+    let zone_id = match args.zone.zone_id {
+        Some(zone_id) => zone_id,
+        None => match client.get_zone_id(&args.zone.zone_name) {
+            Ok(zone_id) => zone_id,
+            Err(err) => {
+                eprintln!("Could not get zone ID: {}", err);
+                return ExitCode::FAILURE;
+            }
+        },
     };
 
-    println!("zone id: {zone_id}");
+    if args.debug {
+        eprintln!("zone id: {zone_id}");
+    }
 
+    let record_id = match (args.record.record_name, args.record.record_id) {
+        (Some(record_name), _) => {
+            let full_record_name = format!("{}.{}", record_name, args.zone.zone_name);
+            match client.get_record_id(&zone_id, &full_record_name) {
+                Ok(record_id) => record_id,
+                Err(err) => {
+                    eprintln!("Could not get record ID: {}", err);
+                    return ExitCode::FAILURE;
+                }
+            }
+        }
+        (_, Some(record_id)) => record_id,
+        _ => unreachable!("Clap should ensure either record_name or record_id is provided."),
+    };
+
+    if args.debug {
+        eprintln!("record id: {record_id}");
+    }
+
+    let record = match client.update_record(&zone_id, &record_id, ip) {
+        Ok(record) => record,
+        Err(err) => {
+            eprintln!("Could not update record: {}", err);
+            return ExitCode::FAILURE;
+        }
+    };
+
+    println!("{} => {ip}", record.name);
     ExitCode::SUCCESS
 }
